09d12075be8e137d4d656b8eba9c11e13a561d0c
skycode <sikiruyaya1@gmail.com>
2023-09-29 02:50:38 -0700

    returns a lists of integers representing the pascal triangle of n

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,14 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
+    """
+    returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
     result = []
 
     if n <= 0:
